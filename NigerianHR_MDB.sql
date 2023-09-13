f233630287ca7814b8ef54d6ced68ccbad197401
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2023-09-13 16:50:32 +0100

    updating

--- NigerianHR_MDB.sql
+++ NigerianHR_MDB.sql
@@ -1,12 +1,59 @@
-CREATE TABLE "Departments" (
-  "id" integer PRIMARY KEY,
-  "EmployeeID" "int(Foreign key, refrences Employees)",
-  "orders" varchar,
-  "marketing" varchar,
-  "accounting" varchar,
-  "administration" varchar,
-  "created_at" timestamp
+DROP TABLE IF EXISTS departments CASCADE;
+
+CREATE TABLE departments (
+  departments_id SERIAL PRIMARY KEY,
+   orders  varchar(255) DEFAULT NULL,
+   marketing  varchar(255) DEFAULT NULL,
+   accounting  varchar(255) DEFAULT NULL,
+   administration  varchar(255) DEFAULT NULL,
+   CONSTRAINT fk_supplier FOREIGN KEY(employees_id) REFERENCES employees(employees_id),
+   created_at TIMESTAMP DEFAULT NOW()
 );
+
+INSERT INTO departments
+(orders, marketing,accounting,administration ) 
+VALUES
+ ('Sales', 'Advertising', 'Finance', 'HR'),
+  ('Shipping', 'Digital Marketing', 'Audit', 'Legal'),
+  ('Customer Service', 'Public Relations', 'Taxation', 'Facilities');
+
+DROP TABLE IF EXISTS departments CASCADE;
+
+CREATE TABLE employees (
+  employees_id SERIAL PRIMARY KEY,
+   fname  varchar(255) DEFAULT NULL,
+  lname  varchar(255) DEFAULT NULL,
+  age  int DEFAULT NULL,
+  contact_add  varchar(255) DEFAULT NULL,
+  emp_email  varchar(255) DEFAULT NULL,
+  full_time  varchar(255) DEFAULT NULL,
+  part_time  varchar(255) DEFAULT NULL,
+  
+  CONSTRAINT fk_training_sessions FOREIGN KEY(training_sessions_id) REFERENCES training_sessions(training_sessions_id),
+  CONSTRAINT fk_multiple_leaves FOREIGN KEY(multiple_leaves_id) REFERENCES multiple_leaves(multiple_leaves_id),
+  CONSTRAINT fk_job_positions FOREIGN KEY(job_positions_id) REFERENCES job_positions(job_positions_id),
+
+   created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO employees
+(   fname,lname , age,contact_add,emp_email , full_time ,part_time ) 
+VALUES
+  ('John', 'Doe', 30, '123 Main St', 'john@example.com', 1, 0),
+  ('Jane', 'Smith', 25, '456 Elm St', 'jane@example.com', 1, 0),
+  ('Bob', 'Johnson', 35, '789 Oak St', 'bob@example.com', 0, 1);
+
+
+
+-- CREATE TABLE "Departments" (
+--   "id" SERIAL PRIMARY KEY,
+--   "EmployeeID" "int(Foreign key, refrences Employees)",
+--   "orders" varchar,
+--   "marketing" varchar,
+--   "accounting" varchar,
+--   "administration" varchar,
+--   "created_at" timestamp
+-- );
 
 CREATE TABLE "Employees" (
   "id" integer PRIMARY KEY,
@@ -61,9 +108,12 @@ CREATE TABLE "Attendance" (
   "created_at" timestamp
 );
 
-ALTER TABLE "Employees" ADD FOREIGN KEY ("id") REFERENCES "Departments" ("id");
+-- ALTER TABLE "employees" ADD FOREIGN KEY ("employees_id") REFERENCES "departments" ("departments_id");
 
-ALTER TABLE "Departments" ADD FOREIGN KEY ("EmployeeID") REFERENCES "Employees" ("id");
+ALTER TABLE "fk_supplier" ADD FOREIGN KEY("employees_id") REFERENCES "employees" ("employees_id");
+ ALTER TABLE "fk_training_sessions" ADD FOREIGN KEY("training_sessions_id") REFERENCES "training_sessions" ("training_sessions_id"),
+ ALTER TABLE "fk_multiple_leaves" ADD FOREIGN KEY("multiple_leaves_id") REFERENCES "multiple_leaves" ("multiple_leaves_id"),
+  ALTER TABLE "fk_job_positions" ADD FOREIGN KEY("job_positions_id") REFERENCES "job_positions" ("job_positions_id"),
 
 ALTER TABLE "Employees" ADD FOREIGN KEY ("id") REFERENCES "JobPositions" ("id");
 
