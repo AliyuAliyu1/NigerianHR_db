613eeee1f64b518601507d6ee2488e056390ae87
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2023-09-14 16:53:09 +0100

    updating

--- view _file.sql
+++ view _file.sql
@@ -8,9 +8,18 @@ SELECT
     e.emp_email,
     e.full_time,
     e.part_time,
-    a.attendance_id AS leaves,
-    a.multiple_leaves AS absences
+    COALESCE(SUM(leaves_per_day), 0) AS Leaves
 FROM employees e
-LEFT JOIN attendance a ON e.employees_id = a.employeeID;
+LEFT JOIN (
+    SELECT
+        employeeID,
+        CASE
+            WHEN monday OR tuesday OR wednesday OR thursday OR friday OR saturday OR sunday THEN 1
+            ELSE 0
+        END AS leaves_per_day
+    FROM attendance
+) a ON e.employees_id = a.employeeID
+GROUP BY e.employees_id, e.fname, e.lname, e.age, e.contact_add, e.emp_email, e.full_time, e.part_time;
+
 
 SELECT * FROM employee_summary;
