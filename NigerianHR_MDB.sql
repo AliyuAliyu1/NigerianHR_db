4e572e7d1633a00b01c386ec995ae1cf438df965
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2023-09-18 14:11:11 +0100

    updating

--- NigerianHR_MDB.sql
+++ NigerianHR_MDB.sql
@@ -6,125 +6,133 @@ CREATE TABLE employees (
   fname  varchar(255) DEFAULT NULL,
   lname  varchar(255) DEFAULT NULL,
   age  int DEFAULT NULL,
-  contact_add  varchar(255) DEFAULT NULL,
+  contact_add  int DEFAULT NULL,
   emp_email  varchar(255) DEFAULT NULL,
-  full_time  boolean DEFAULT NULL,
-  part_time  boolean DEFAULT NULL,
-
-training_sessions_id int,
-multiple_leaves_id int,
-job_positions_id int, 
-  CONSTRAINT training_sessions_fk FOREIGN KEY (training_sessions_id) REFERENCES training_sessions(training_sessions_id),
-  CONSTRAINT multiple_leaves_fk FOREIGN KEY (multiple_leaves_id) REFERENCES multiple_leaves(multiple_leaves_id),
-  CONSTRAINT job_positions_fk FOREIGN KEY (job_positions_id) REFERENCES job_positions(job_positions_id),
+  work_type  varchar(255) DEFAULT NULL,
   
-
- 
+  attendance_ID int,
+  department_ID int,
+  job_positions_ID int, 
+  CONSTRAINT training_sessions_fk FOREIGN KEY (attendance_ID) REFERENCES attendance(attendance_id),
+  CONSTRAINT multiple_leaves_fk FOREIGN KEY (department_ID) REFERENCES departments(departments_id),
+  CONSTRAINT job_positions_fk FOREIGN KEY (job_positions_ID) REFERENCES job_positions(job_positions_id),
+  
    created_at TIMESTAMP DEFAULT NOW()
 );
 
 INSERT INTO employees
-(   fname,lname , age,contact_add,emp_email , full_time ,part_time ) 
+( fname,lname , age,contact_add,emp_email , work_type) 
 VALUES
-  ('John', 'Doe', 30, '123 Main St', 'john@example.com', TRUE, TRUE),
-  ('Jane', 'Smith', 25, '456 Elm St', 'jane@example.com', TRUE, FALSE),
-  ('Bob', 'Johnson', 35, '789 Oak St', 'bob@example.com', FALSE, TRUE);
-
+  ('John', 'Doe', 30, 090, 'john@example.com', 'full time'),
+  ('Jane', 'Smith', 25, 54, 'jane@example.com', 'part time'),
+  ('Bob', 'Johnson', 35, 24, 'bob@example.com', 'full time');
 
   DROP TABLE IF EXISTS departments CASCADE;
 
 CREATE TABLE departments (
-  departments_id SERIAL PRIMARY KEY,
-   orders  varchar(255) DEFAULT NULL,
-   marketing  varchar(255) DEFAULT NULL,
-   accounting  varchar(255) DEFAULT NULL,
-   administration  varchar(255) DEFAULT NULL,
+   departments_id SERIAL PRIMARY KEY,
+   department_name varchar(255) DEFAULT NULL,
    employeeID int,
  CONSTRAINT employee_fk FOREIGN KEY(employeeID) REFERENCES employees(employees_id),
-
-
   
    created_at TIMESTAMP DEFAULT NOW()
 );
 
 INSERT INTO departments
-(orders, marketing,accounting,administration ) 
+(department_name) 
 VALUES
- ('Sales', 'Advertising', 'Finance', 'HR'),
-  ('Shipping', 'Digital Marketing', 'Audit', 'Legal'),
-  ('Customer Service', 'Public Relations', 'Taxation', 'Facilities');
+ ('Sales'),
+  ('Shipping'),
+  ('Accounting'),
+  ('Customer Service');
 
 DROP TABLE IF EXISTS job_positions CASCADE;
 
+
+
 CREATE TABLE job_positions (
   job_positions_id SERIAL PRIMARY KEY,
-  accountants varchar(255) DEFAULT NULL,
-  sales_reps varchar(255) DEFAULT NULL,
-  dispatch_riders varchar(255) DEFAULT NULL,
-  administrators varchar(255) DEFAULT NULL,
+  position_name varchar(255) DEFAULT NULL,
 
-  
    created_at TIMESTAMP DEFAULT NOW()
 );
 
 INSERT INTO job_positions
-( accountants,sales_reps,dispatch_riders,administrators) 
+( position_name) 
 VALUES
-('Ahmed', 'Tomas', 'Johna', 'favour'),
-('David', 'Emmanuel', 'jude', 'Tolu'),
-('Bivan', 'Aroh', 'Saleem', 'Hameed'),
-('Tope', 'Mikail', 'Ochiba', 'Excel');
+('Accountant'),
+('Marketer'),
+('Designer'),
+('Manager');
 
+DROP TABLE IF EXISTS full_time CASCADE;
 
-DROP TABLE IF EXISTS pay_roll CASCADE;
-
-CREATE TABLE pay_roll (
-  pay_roll_id SERIAL PRIMARY KEY,
-  hourly_rate int DEFAULT NULL,
-  monthly_rate int DEFAULT NULL,
+CREATE TABLE full_time (
+  full_timeid SERIAL PRIMARY KEY,
+  monthly_rates int DEFAULT NULL,
+  employeeID int,
+  CONSTRAINT employee_fk FOREIGN KEY(employeeID) REFERENCES employees(employees_id),
 
    created_at TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO pay_roll
-( hourly_rate,monthly_rate) 
+INSERT INTO full_time
+( monthly_rates) 
 VALUES
-(20.50, 4000.00),
-(18.75, 3800.00),
-(22.00, 4200.00);
+( 4000),
+( 3800),
+( 4200);
 
-DROP TABLE IF EXISTS activities CASCADE;
+DROP TABLE IF EXISTS part_time CASCADE;
 
-CREATE TABLE activities (
-  activities_id SERIAL PRIMARY KEY,
-  attendance boolean DEFAULT NULL,
-  multiple_leaves int DEFAULT NULL,
-  training_sessions int DEFAULT NULL,
+CREATE TABLE part_time (
+  part_timeid SERIAL PRIMARY KEY,
+
+  hourly_rates int DEFAULT NULL,
+  employeeID int,
+  CONSTRAINT employee_fk FOREIGN KEY(employeeID) REFERENCES employees(employees_id),
+
+   created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO part_time
+(hourly_rates) 
+VALUES
+(20),
+(18),
+(22);
+
+
+
+DROP TABLE IF EXISTS leaves CASCADE;
+
+CREATE TABLE leaves (
+
+  leaves_id SERIAL PRIMARY KEY,
+  leave_reason varchar(255) DEFAULT NULL,
+  leave_start_date varchar(255) DEFAULT NULL,
+  leave_end_date varchar(255) DEFAULT NULL,
    employeeID int,
  CONSTRAINT employee_fk FOREIGN KEY(employeeID) REFERENCES employees(employees_id),
 
-  
    created_at TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO activities
-(attendance,multiple_leaves,training_sessions) 
+INSERT INTO leaves
+(leave_reason,leave_start_date,leave_end_date) 
 VALUES
-(TRUE, 2, 4),
-(FALSE, 3, 6),
-(TRUE, 1, 0);
+('sick', '12/1/2022', '12/4/2022'),
+('emergency', '12/2/2022', '12/3/2022'),
+('visit', '12/8/2022', '12/9/2022');
 
 DROP TABLE IF EXISTS attendance CASCADE;
 
 CREATE TABLE attendance (
   attendance_id SERIAL PRIMARY KEY,
-  monday boolean DEFAULT NULL,
-  tuesday boolean DEFAULT NULL,
-  wednesday boolean DEFAULT NULL,
-  thursday boolean DEFAULT NULL,
-  friday boolean DEFAULT NULL,
-  saturday boolean DEFAULT NULL,
-  sunday boolean DEFAULT NULL,
+ 
+  attendance_date varchar(255) DEFAULT NULL,
+  is_present boolean DEFAULT NULL,
+
   employeeID int,
  CONSTRAINT employee_fk FOREIGN KEY(employeeID) REFERENCES employees(employees_id),
 
@@ -133,8 +141,8 @@ CREATE TABLE attendance (
 );
 
 INSERT INTO attendance
-(  monday,tuesday,wednesday,thursday,friday,saturday,sunday) 
+( attendance_date,is_present) 
 VALUES
-(TRUE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE),
-(FALSE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE),
-(TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE);
+('12/4/2022', TRUE),
+('12/2/2022', TRUE),
+('12/8/2022', FALSE);
--- raw_file.sql
+++ raw_file.sql
@@ -1,4 +1,4 @@
-
+/* 
 
 
 CREATE TABLE "employees" (
@@ -94,3 +94,83 @@ ALTER TABLE "attendance" ADD FOREIGN KEY ("attendance_id") REFERENCES "employees
 ALTER TABLE "employees" ADD FOREIGN KEY ("employees_id") REFERENCES "attendance" ("employeeID");
 
 ALTER TABLE "departments" ADD FOREIGN KEY ("employeeID") REFERENCES "employees" ("employees_id");
+ */
+
+ CREATE TABLE "departments" (
+  "departments_id" integer PRIMARY KEY,
+  "department_name" varchar,
+  "created_at" timestamp
+);
+
+CREATE TABLE "employees" (
+  "employees_id" integer PRIMARY KEY,
+  "fname" varchar,
+  "lname" varchar,
+  "age" integer,
+  "contact_add" int,
+  "emp_email" varchar,
+  "work_type" varchar,
+  "attendanceID" "int(Foreign key, refrences attendance)",
+  "departmentID" "int(Foreign key, refrences departments)",
+  "job_positionsID" "int(Foreign key, refrences job_positions)",
+  "created_at" timestamp
+);
+
+CREATE TABLE "job_positions" (
+  "job_positions_id" integer PRIMARY KEY,
+  "position_name" varchar,
+  "created_at" timestamp
+);
+
+CREATE TABLE "full_time" (
+  "full_timeid" integer PRIMARY KEY,
+  "monthly_rates" int,
+  "employeeID" "int(Foreign key, refrences employees)",
+  "created_at" timestamp
+);
+
+CREATE TABLE "part_time" (
+  "part_timeid" integer PRIMARY KEY,
+  "hourly_rates" int,
+  "employeeID" "int(Foreign key, refrences employees)",
+  "created_at" timestamp
+);
+
+CREATE TABLE "leaves" (
+  "leaves_id" integer PRIMARY KEY,
+  "leave_reason" varchar,
+  "leave_start_date" varchar,
+  "leave_end_date" varchar,
+  "employeeID" "int(Foreign key, refrences employees)",
+  "created_at" timestamp
+);
+
+CREATE TABLE "training_sessions" (
+  "training_sessions_id" integer PRIMARY KEY,
+  "session_name" varchar,
+  "session_date" varchar,
+  "employeeID" "int(Foreign key, refrences employees)",
+  "created_at" timestamp
+);
+
+CREATE TABLE "attendance" (
+  "attendance_id" integer PRIMARY KEY,
+  "attendance_date" varchar,
+  "is_present" Boolean,
+  "employeeID" "int(Foreign key, refrences employees)",
+  "created_at" timestamp
+);
+
+ALTER TABLE "employees" ADD FOREIGN KEY ("departmentID") REFERENCES "departments" ("departments_id");
+
+ALTER TABLE "employees" ADD FOREIGN KEY ("job_positionsID") REFERENCES "job_positions" ("job_positions_id");
+
+ALTER TABLE "employees" ADD FOREIGN KEY ("attendanceID") REFERENCES "attendance" ("attendance_id");
+
+ALTER TABLE "leaves" ADD FOREIGN KEY ("employeeID") REFERENCES "employees" ("employees_id");
+
+ALTER TABLE "training_sessions" ADD FOREIGN KEY ("employeeID") REFERENCES "employees" ("employees_id");
+
+ALTER TABLE "partime_time" ADD FOREIGN KEY ("employeeID") REFERENCES "employees" ("employees_id");
+
+ALTER TABLE "full_time" ADD FOREIGN KEY ("employeeID") REFERENCES "employees" ("employees_id");
